e6670b88a1d3d2f2a022d51dbf59fa38dcbb6487
hanansabry <tweety624>
2022-02-27 11:55:30 +0200

    Initial commit

---
+++ app/src/main/java/com/app/cms/presentation/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.app.cms.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.app.cms.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, StartActivity.class));
+        }, SPLASH_TIME_OUT);
+    }
+}
---
+++ app/src/main/java/com/app/cms/presentation/StartActivity.java
@@ -0,0 +1,16 @@
+package com.app.cms.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.cms.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
